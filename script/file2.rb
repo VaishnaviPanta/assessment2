8d348f25f7b1c56e0c4b13c8b8eaf815019900c9
VAISHNAVI <vaishnavi@dispatchtrack.com>
2024-07-05 13:21:45 +0530

    task5 added

---
+++ script/file2.rb
@@ -0,0 +1,20 @@
+def max_number(num1, num2, num3, num4)
+    max_value = num1
+          
+    if num2 > max_value
+      max_value = num2
+    end
+          
+    if num3 > max_value
+      max_value = num3
+    end
+          
+    if num4 > max_value
+      max_value = num4
+    end
+    result = 'This is the code to find maximum value among four numbers, this has
+              to be correct efficiently following all code standards, you can rewrite the
+              logic or modify the existing code but do not use simplified methods, write raw
+              logic'
+  end
+  
