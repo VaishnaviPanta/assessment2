1b924bcba0d258b6f77fc1536577c144dba2c25f
VAISHNAVI <vaishnavi@dispatchtrack.com>
2024-07-26 13:17:29 +0530

    team_model added

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,9 @@
 class Team < ApplicationRecord
     has_many :players
+    has_many :matches
     validates :name, presence:true, uniqueness:true
     validates :country, presence:true
-    validates :founded, presence:true
+    validates :founded, presence:true, numericality: { only_integer: true, greater_than: 1901 }
+    scope :from_country, ->(country){where(country: country)}
+    scope :founded_after, ->(year){where('founded_year > ?',(year))}
 end
