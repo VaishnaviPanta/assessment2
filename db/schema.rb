c87d327591784988662a70e54051b2b85281ff41
VAISHNAVI <vaishnavi@dispatchtrack.com>
2024-07-26 13:20:39 +0530

    schema file added

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_081405) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_26_072923) do
   create_table "matches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.date "date"
     t.string "location"
-    t.integer "team1_id"
-    t.integer "team2_id"
-    t.integer "score_team1"
-    t.integer "score_team2"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "home_team_id"
+    t.integer "away_team_id"
   end
 
   create_table "players", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.string "position"
-    t.bigint "team_id", null: false
+    t.string "role"
+    t.boolean "is_captain"
+    t.boolean "is_active"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role", null: false
-    t.boolean "is_captain", default: false, null: false
-    t.boolean "is_active", default: true, null: false
-    t.text "description"
-    t.index ["team_id"], name: "index_players_on_team_id"
+    t.integer "team_id"
+    t.bigint "teams_id"
+    t.integer "home_team_id"
+    t.integer "away_team_id"
+    t.index ["team_id"], name: "fk_rails_8880a915a5"
+    t.index ["teams_id"], name: "index_players_on_teams_id"
   end
 
   create_table "teams", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -41,7 +43,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_081405) do
     t.string "country", limit: 30, null: false
     t.integer "founded"
     t.text "description"
+    t.integer "player_count"
+    t.integer "home_team_id"
+    t.integer "away_team_id"
+    t.index ["away_team_id"], name: "index_teams_on_away_team_id", unique: true
+    t.index ["home_team_id"], name: "index_teams_on_home_team_id", unique: true
     t.index ["name"], name: "name", unique: true
   end
 
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "players", "teams"
 end
