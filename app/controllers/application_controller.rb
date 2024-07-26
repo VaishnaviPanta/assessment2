3b1873b316c2fb0196702c4acf70aba7449961b5
VAISHNAVI <vaishnavi@dispatchtrack.com>
2024-07-26 13:14:24 +0530

    application_controller added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    include pundit
+    before_action :authenticate_user!
+rescue_from 
+  pundit::NotAuthorizedError,
+  with: :user_not_authorized
+private
+def user_not_authorized
+    flash[:alert] = "Your are not authorized to perform this action."
+    redirect_to(root_path)
+end
 end
