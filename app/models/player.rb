287ec54585b1be9fe0228efeb4b3fb9208864cfc
VAISHNAVI <vaishnavi@dispatchtrack.com>
2024-07-26 13:16:32 +0530

    player_model added

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,10 @@
 class Player < ApplicationRecord
     belongs_to :team
+    belongs_to :match
+    before_destroy :not_captain
+    after_create :increment_team_player_count
+    after_destroy :derement_team_player_count
+    validates :single_captain_per_team
     validates :name, presence: true
     validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :position, presence: true
@@ -8,4 +13,26 @@ class Player < ApplicationRecord
     validates :is_captain, inclusion: { in: [true, false] }
     validates :is_active, inclusion: { in: [true, false] }
     enum role: { allrounder: 'allrounder', bowler: 'bowler', batsman: 'batsman' }
+    default_scope {where(active: true)}
+    scope :with_role, ->(role) {where(position:role)}
+    private
+    def not_captain
+      if is_captain
+        errors.add(:base, 'cannot delete a player who is a captain')
+        throw :abort
+      end
+    end
+    def increment_team_player_count
+      team.increment!(:player_count)
+    end
+    def decrement_team_player_count
+      team.decrement!(:player_count)
+    end
+    def single_captain_per_team
+      if is_captain && team.players.where(is_captain: true).exists?
+        errors.add(:is_captain, 'A team can only have one captain')
+      end
+    end
+  end
+
   end
